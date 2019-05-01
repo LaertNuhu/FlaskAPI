71c1020ee2948e43ef4c4c99ff1c657f3dfd9c97
Laert Nuhu <laert.nuhu@campus.tu-berlin.de>
2019-05-01 17:13:22 +0200

    (task) separate the network generation logic into a separate class

---
+++ generateNetwork.py
@@ -0,0 +1,66 @@
+from dataManipulation import XMLDataframeParser
+from normalize import Normalizer
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import networkx as nx
+from networkx.readwrite import json_graph
+import pandas as pd
+import functools
+from nltk.util import skipgrams
+from toolz import itertoolz, compose
+from toolz.curried import map as cmap, sliding_window, pluck
+
+class NetworkGenerator:
+
+    def generate(self, edgeCount, tfidf = False, window_size = 0):
+        parser = XMLDataframeParser()
+        text = parser.getText("./data/smokingRecords.xml")
+        parser.addFeatureFromText(text, "HISTORY OF PRESENT ILLNESS :", "", True, True, "illness")
+        df = parser.getDataframe()
+        df_xml = parser.removeEmptyEntries(df, "illness")
+        normalizer = Normalizer()
+        if tfidf:
+            if window_size == 0:
+                vectorizer = TfidfVectorizer(tokenizer = lambda text: normalizer.normalize(text, True, False), ngram_range = (2, 2))
+                mostFreq2Grams = self.get_first_n_words(vectorizer, df_xml.illness, edgeCount)
+            else:
+                vectorizer = TfidfVectorizer(analyzer = lambda text: self.custom_analyser(text, 2, int(window_size)))
+                mostFreq2Grams = self.get_first_n_words(vectorizer, normalizer.normalizeArray(df_xml.illness, True, False), edgeCount)
+        else:
+            if window_size == 0:
+                vectorizer = CountVectorizer(tokenizer = lambda text: normalizer.normalize(text, True, False), ngram_range = (2, 2))
+                mostFreq2Grams = self.get_first_n_words(vectorizer, df_xml.illness, edgeCount)
+            else:
+                vectorizer = CountVectorizer(analyzer = lambda text: self.custom_analyser(text, 2, int(window_size)))
+                mostFreq2Grams = self.get_first_n_words(vectorizer, normalizer.normalizeArray(df_xml.illness, True, False), edgeCount)
+        df_graph = self.create_dataframe(mostFreq2Grams)
+        GF = nx.from_pandas_edgelist(df_graph, 'Node1', 'Node2', ["Weight"])
+        return json_graph.node_link_data(GF)
+
+    def custom_analyser(self, token, n, k):
+        return compose(cmap(' '.join), skipgrams)(token, n, k)
+
+    def get_first_n_words(self, vec, data, n):
+        bag_of_words = vec.fit_transform(data)
+        sum_words = bag_of_words.sum(axis = 0)
+        words_freq = [(word, sum_words[0, idx])
+                    for word, idx in vec.vocabulary_.items()]
+        words_freq_sorted = sorted(words_freq, key = lambda x: x[1], reverse = True)
+        return words_freq_sorted[:n]
+
+
+    def create_dataframe(self, data):
+        node1 = []
+        node2 = []
+        weight = []
+        for entry in data:
+            nodes = entry[0].split()
+            node1.append(nodes[0])
+            node2.append(nodes[1])
+            weight.append(entry[1])
+        df = pd.DataFrame()
+        df['Node1'] = node1
+        df['Node2'] = node2
+        df['Weight'] = weight
+        return df
